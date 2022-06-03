0829fccb3f4c267c88ce45754b38a2b1caad1952
christopherchinia <chinia214@gmail.com>
2022-06-03 11:43:50 -0400

    Added the backgoround image modified the js file added a section container in the html modified the css file

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -2,24 +2,29 @@ const container = document.querySelector(".container"),
 eyeShow = document.querySelectorAll(".showEye"),
 pwFields = document.querySelectorAll(".password");
 
+
 //  js code for show/hide password and icon change
 
 eyeShow.forEach(eyeIcon =>{
-     eyeIcon.addEventListener("click", ()=>{
-        pwFields.forEach(pwField =>{
-            if(pwField.type ==="password"){
-                pwField.type = "text";
+    eyeIcon.addEventListener("click", ()=>{
+       pwFields.forEach(pwField =>{
+           if(pwField.type ==="password"){
+               pwField.type = "text";
 
-                eyeShow.forEach(icon =>{
-                    icon.classList.replace("uil-eye-slash","uil-eye");
-                })  
-            }else{
-                pwField.type = "password";
+               eyeShow.forEach(icon =>{
+                   icon.classList.replace("uil-eye-slash","uil-eye");
+               })  
+           }else{
+               pwField.type = "password";
 
-                eyeShow.forEach(icon =>{
-                    icon.classList.replace("uil-eye","uil-eye-slash");
-                }) 
-            }
-        })
-     })
+               eyeShow.forEach(icon =>{
+                   icon.classList.replace("uil-eye","uil-eye-slash");
+               }) 
+           }
+       })
+    })
 })
+
+
+
+
